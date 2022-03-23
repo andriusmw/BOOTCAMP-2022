23bb8e94e5cbf8e2c71a96402a9a8fbb51a14c7e
andriusmw <andriusmw@yahoo.es>
2022-03-23 13:44:07 +0100

    función multiplicar y dividir

--- katas/kata calculadora/js/calc.js
+++ katas/kata calculadora/js/calc.js
@@ -19,3 +19,30 @@
     var result = A - B;
     alert("resultado RESTA: " + result);
 }
+
+function multiplicar() {
+    var A = document.getElementById("operadorA").value ;
+    var B = document.getElementById("operadorB").value ;
+    A = +A;  //toNumnber
+    alert("var A:" + A);
+    B = +B;   //toNumber
+    alert("var B:" + B);
+    var result = A * B;
+    alert("resultado multiplicar: " + result);
+}
+
+
+function dividir() {
+    var A = document.getElementById("operadorA").value ;
+    var B = document.getElementById("operadorB").value ;
+    A = +A;  //toNumnber
+    alert("var A:" + A);
+    B = +B;   //toNumber
+    alert("var B:" + B);
+    var cociente = A / B;
+   // var resto = A % B;
+    alert("cociente división: " + cociente);
+   // alert("resto división: " + resto); 
+   //lo divide de forma exacta de modo que el resto no es fiable.
+}
+
