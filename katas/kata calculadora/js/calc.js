57bbe57deb7513ff0e544cfea8e164fc4c235bb7
andriusmw <andriusmw@yahoo.es>
2022-03-23 13:37:56 +0100

    función suma

---
+++ katas/kata calculadora/js/calc.js
@@ -0,0 +1,10 @@
+ function suma() {
+     var A = document.getElementById("operadorA").value ;
+     var B = document.getElementById("operadorB").value ;
+     A = +A;  //toNumnber
+     alert("var A:" + A);
+     B = +B;   //toNumber
+     alert("var B:" + B);
+     var result = A + B;
+     alert("resultado SUMA: " + result);
+ }
