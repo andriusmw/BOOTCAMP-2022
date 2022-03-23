98ebf69b87dbfcac546fa98dbfefcbffad8a8e0d
andriusmw <andriusmw@yahoo.es>
2022-03-23 13:39:08 +0100

    función resta

--- katas/kata calculadora/js/calc.js
+++ katas/kata calculadora/js/calc.js
@@ -8,3 +8,14 @@
      var result = A + B;
      alert("resultado SUMA: " + result);
  }
+
+ function resta() {
+    var A = document.getElementById("operadorA").value ;
+    var B = document.getElementById("operadorB").value ;
+    A = +A;  //toNumnber
+    alert("var A:" + A);
+    B = +B;   //toNumber
+    alert("var B:" + B);
+    var result = A - B;
+    alert("resultado RESTA: " + result);
+}
