b7c8161d60904ce0630c8aadedd1ad4ec595ffb5
andriusmw <andriusmw@yahoo.es>
2022-03-24 16:36:53 +0100

    crearTarea funcional hay que mirar cosas

--- katas/kata añadirelementos Andrius/js/index.js
+++ katas/kata añadirelementos Andrius/js/index.js
@@ -5,20 +5,12 @@ function crearTarea() {
 
   
 
-    let  NuevaTarea =  document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
+   return  (document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
     + NombreTarea + "<br><br><span>Descripción</span><br><br>" 
     + DescripTarea + "</div>")
     //Escribe el div y pone el titulo y la descripcion
-    //Luego lo hemos asignadon a una variable con intención de poder pasarle todo
-    //a otro div que elijamos por id y que salga dentro de la misma página sin recargar
-    //pero no chuta :/ 
-
-    let nuediv = document.getElementById("listatareas");
-    nuediv.appendchild(NuevaTarea);
+)}
+  
 
 
 
-
-
-
-}
