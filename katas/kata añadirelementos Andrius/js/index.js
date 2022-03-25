6b8e73611db238b234da18fc0a73f513a76a94f4
andriusmw <andriusmw@yahoo.es>
2022-03-25 12:23:39 +0100

    cambiando javascript añadir elementos arrow function event listener

--- katas/kata añadirelementos Andrius/js/index.js
+++ katas/kata añadirelementos Andrius/js/index.js
@@ -1,15 +1,30 @@
-function crearTarea() {
-    let NombreTarea = document.getElementById("nombretarea").value;
-    let DescripTarea = document.getElementById("descriptarea").value;
-    //recibe datos del index
+//function crearTarea(e) {
+    const crearTarea = document.getElementById("addbtn")
 
-  
+    crearTarea.addEventListener("click", (e) =>{
+        e.preventDefault();
+        console.log("inicio")
+        let NombreTarea = document.getElementById("nombretarea").value;
+        let DescripTarea = document.getElementById("descriptarea").value;
+        //recibe datos del index
+    
+      
+    
+      const NuevaTarea = document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
+        + NombreTarea + "<br><br><span>Descripción</span><br><br>" 
+        + DescripTarea + "</div>")
+        //Escribe el div y pone el titulo y la descripcion
+    
+        const divtareas = document.getElementById("listatareas");
+        divtareas.appendChild(NuevaTarea);
+        //Seleciona el div listatareas y crea un elemento hijo llamado NuevaTarea
+        //osea debe de meter ahí el div creado con la tarea.
+    
+        console.log("ha recorrido todo")
+    
+    })
 
-   return  (document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
-    + NombreTarea + "<br><br><span>Descripción</span><br><br>" 
-    + DescripTarea + "</div>")
-    //Escribe el div y pone el titulo y la descripcion
-)}
+    
   
 
 
