93df18836b0a472c86eb0421a2c51afdae7a1541
andriusmw <andriusmw@yahoo.es>
2022-03-25 12:49:38 +0100

    muestra titulo y descripcion donde queremos, funciona correctamente, falta añadir clases

--- katas/kata añadirelementos Andrius/js/index.js
+++ katas/kata añadirelementos Andrius/js/index.js
@@ -14,21 +14,30 @@
         const NuevoSpan = document.createElement("span");
         NuevoSpan.textContent = "Título Tarea";
         const p = document.createElement("p")
-        const pnombretarea = NombreTarea.value;
-        p.textContent = pnombretarea;
-        
+        const pnombretarea = NombreTarea; //Crea una constante para el valor
+        p.textContent = pnombretarea;// le pasa el valor como contenido de texto
+
+        //Crea el div mete el span, le pasa el valor al span, crea el P donde va a ir el valor del titulo de la tarea
+        //es decir NombreTarea y se lo pasa.
+
+        const NuevoSpan2 = document.createElement("span");
+        NuevoSpan2.textContent = "Descripción";
+        const p2 = document.createElement("p")
+        const p2Descrip = DescripTarea;
+        p2.textContent = p2Descrip;
+
 
         NuevaTarea.appendChild(NuevoSpan);
             NuevaTarea.appendChild(p);
+         //Indica que meta el span, dentro del div que ha creado y luego que meta el p.   
+         NuevaTarea.appendChild(NuevoSpan2);
+         NuevaTarea.appendChild(p2);
+
 
         /*/  LO QUE PASABA ERA QUE DOCUMENT.WRITE VOLVIA A ESCRIBIR EN EL DOCUMENTO Y CLARO, ya no habia div listatareas donde añadir nada
         ni nada de lo anterior, machacaba todo y escribia el div que yo le habia dicho. Hay que usar document.createElement*/
 
-    /*
-      const NuevaTarea = document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
-        + NombreTarea + "<br><br><span>Descripción</span><br><br>" 
-        + DescripTarea + "</div>")
-        //Escribe el div y pone el titulo y la descripcion */
+
     
         const divtareas = document.getElementById("listatareas");
         divtareas.appendChild(NuevaTarea);
