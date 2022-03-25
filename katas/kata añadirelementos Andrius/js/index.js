94fff167f4a703ec3f1ab75a3743a1e6d5f37744
andriusmw <andriusmw@yahoo.es>
2022-03-25 12:36:28 +0100

    usando document.element encajando divs

--- katas/kata añadirelementos Andrius/js/index.js
+++ katas/kata añadirelementos Andrius/js/index.js
@@ -9,11 +9,26 @@
         //recibe datos del index
     
       
-    
+
+        const NuevaTarea = document.createElement("div");
+        const NuevoSpan = document.createElement("span");
+        NuevoSpan.textContent = "Título Tarea";
+        const p = document.createElement("p")
+        const pnombretarea = NombreTarea.value;
+        p.textContent = pnombretarea;
+        
+
+        NuevaTarea.appendChild(NuevoSpan);
+            NuevaTarea.appendChild(p);
+
+        /*/  LO QUE PASABA ERA QUE DOCUMENT.WRITE VOLVIA A ESCRIBIR EN EL DOCUMENTO Y CLARO, ya no habia div listatareas donde añadir nada
+        ni nada de lo anterior, machacaba todo y escribia el div que yo le habia dicho. Hay que usar document.createElement*/
+
+    /*
       const NuevaTarea = document.write("<div class=`nuevatarea`> <span>Título Tarea</span><br><br>"
         + NombreTarea + "<br><br><span>Descripción</span><br><br>" 
         + DescripTarea + "</div>")
-        //Escribe el div y pone el titulo y la descripcion
+        //Escribe el div y pone el titulo y la descripcion */
     
         const divtareas = document.getElementById("listatareas");
         divtareas.appendChild(NuevaTarea);
