a2ddf08026333ef00714c5bafc7f54e38bb472b5
andriusmw <andriusmw@yahoo.es>
2022-03-28 17:01:29 +0200

    funciona drag y drop, muestra texto erroneo pero funciona , revisar lo que muestra

--- katas/kata carrito drag and drop/js/carritodragdrop.js
+++ katas/kata carrito drag and drop/js/carritodragdrop.js
@@ -1,26 +1,61 @@
-//const articulo = document.getElementsByClassName("articulo");
-/*
-articulo.addEventListener("dragstart", e => {
-    e.preventDefault();
-    console.log("drag start");
-});*/
+const articulo = document.querySelector(".articulo");
 
+articulo.addEventListener("dragstart", e => {
+    
+    console.log("drag start");
+});
+/*
 let articulos=document.querySelectorAll('.articulo');
 articulos.forEach((articulo)=>{
+
+    
+    
+
  articulo.addEventListener('dragstart',(event)=>{
 
     console.log("drag start works!")
-  });
-  articulo.addEventListener('dragend',(event)=>{
+  });*/
+ /* articulo.addEventListener('dragend',(event)=>{
 
     console.log("drag End works!")
-  });
-  articulo.addEventListener('drag',(event)=>{
+  });*/
+ /* articulo.addEventListener('drag',(event)=>{
 
     console.log("drag  works!")
-  });
+  });*/
   
-});
+//});*/
 //GETELEMENTBYCLASSNAME crea un array de objetos de todos los resultados encontradoscon esa clase
 // de modo que habría que crear un array de objetos y para cada objeto escucharel evento 
+
+const contendor = document.getElementById("carrito");
+
+contendor.addEventListener("dragenter", e => {
+    console.log("Drag Enter")
+})
+contendor.addEventListener("dragleave", e => {
+    console.log("Drag Leave")
+})
+contendor.addEventListener("dragover", e => {
+    e.preventDefault(); //prevee su ejecución por defecto para que funcione Drop
+    console.log("Drag Over")
+})
+contendor.addEventListener("drop", e => {
+    console.log("Drop")
+    //console.log(articulos)
+    //contendor.appendChild(articulo);
+    const text = document.querySelector(".tituloart").textContent;
+    
+        const divart = document.createElement("div");
+         const tituloart = document.createElement("p");
+         tituloart.textContent = text;
+         console.log(text)
+
+         divart.appendChild(tituloart);
+        
+         
+       
+         contendor.appendChild(divart);
+    
+})
 
